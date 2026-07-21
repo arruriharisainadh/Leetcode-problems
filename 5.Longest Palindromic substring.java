be631bebf1ac7f3cd357336076063005c00380ae
arruriharisainadh <arruriharisainadh@gmail.com>
2026-07-21 13:28:01 +0530

    Create 5.Longest Palindromic substring.java

---
+++ 5.Longest Palindromic substring.java
@@ -0,0 +1,8 @@
+ private int expandAroundCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
+}

b59e9cf65e8ab230251378b6fcbe04ab4afaefc5
arruriharisainadh <arruriharisainadh@gmail.com>
2026-07-27 11:32:14 +0530

    Implement intersection detection for two linked lists

---
+++ 160.Intersection of Tw Linked Lists.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+
+        ListNode pA = headA;
+        ListNode pB = headB;
+
+        // Traverse both lists. When pA reaches the end, redirect it to headB.
+        // When pB reaches the end, redirect it to headA.
+        while (pA != pB) {
+            pA = (pA == null) ? headB : pA.next;
+            pB = (pB == null) ? headA : pB.next;
+        }
+
+        // Returns the intersection node or null if there's no intersection
+        return pA;
+    }
+}
